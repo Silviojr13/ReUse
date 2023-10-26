39a6fefdb00d646015d7aeb394961189442d1e9b
Silviojrtv13 <silviomarcaneiro@gmail.com>
2023-10-25 22:34:50 -0300

    sarve

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost"; // Endereço do servidor de banco de dados
+$username = "root"; // Nome de usuário do banco de dados
+$password = "PUC@1234"; // Senha do banco de dados
+$database = "reuse"; // Nome do banco de dados
+
+// Cria uma conexão com o banco de dados
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+}/*else {
+    echo "+";
+}*/
+
+?>
